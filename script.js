193a3b49814c2f7c83f70b3154c4f0b3273d0e00
thecelestineoracle <celestineguide@proton.me>
2026-08-26 14:06:31 +1000

    Add site interactions

---
+++ script.js
@@ -0,0 +1,1 @@
+const btn=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');btn?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
